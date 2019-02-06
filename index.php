38556f0dacb7dfa1c7cf5495fe4d23d1521597d8
elharony <p.yahyaelharony@gmail.com>
2019-02-06 05:58:33 +0200

    Add static 'intro' section

--- index.php
+++ index.php
@@ -2,6 +2,14 @@
 
 <main>
 
+
+    <section class="intro">
+        <div class="container">
+            <h2 class="section-heading">Looking for a Chalet or Caviar?</h2>
+            <p class="section-description">Realestator is your guide to Buy/Rent Chalets & Caviar in Aspen, Colorado!</p>
+        </div>
+    </section>
+
 <?php the_content(); ?>
 
 </main>
