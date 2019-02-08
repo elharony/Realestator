79ccff62e37d0e257f25ef2b3b29f7173e2f242b
elharony <p.yahyaelharony@gmail.com>
2019-02-08 04:04:11 +0200

    Add 'Contact Us' page with Ninja Forms

---
+++ page-contact.php
@@ -0,0 +1,23 @@
+<?php get_header(); ?>
+
+<main class="page-contact">
+
+    <section class="hero">
+        <div class="container">Contact Us</div>
+    </section>
+
+    <div class="container">
+        <section class="contact-wrapper">
+            <div class="contact-form">
+                <?php echo do_shortcode("[ninja_forms id=1]"); ?>
+            </div>
+            <div class="contact-info">
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3092.258851044348!2d-106.82363128425192!3d39.191566637154786!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8740397c579f3327%3A0x44d72b12089b2faa!2s100-198+N+Aspen+St%2C+Aspen%2C+CO+81611%2C+USA!5e0!3m2!1sen!2seg!4v1549589784207" width="100%" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+            </div>
+        </section>
+    </div>
+
+</main>
+
+
+<?php get_footer(); ?>
