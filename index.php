d390e5b01c685fa51d7d816f8659449a901f60a2
elharony <p.yahyaelharony@gmail.com>
2019-02-08 04:24:34 +0200

    Fix the homepage's buttons routing

--- index.php
+++ index.php
@@ -14,12 +14,12 @@
         <section class="items-sell">
             <h3 class="section-heading">Buy</h3>
             <?php echo do_shortcode('[es_my_listing layout="2_col" category="for sale" posts_per_page="2" show_filter="0"]'); ?>
-            <a href="/buy/" class="btn">See All</a>
+            <a href="<?php echo get_home_url(); ?>/buy/" class="btn">See All</a>
         </section>
         <section class="items-rent">
             <h3 class="section-heading">Rent</h3>
             <?php echo do_shortcode('[es_my_listing layout="2_col" category="for rent" posts_per_page="2" show_filter="0"]'); ?>
-            <a href="/rent/" class="btn">All Rentals</a>
+            <a href="<?php echo get_home_url(); ?>/rent/" class="btn">All Rentals</a>
         </section>
     </section>
 
